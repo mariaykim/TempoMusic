640b2cdbc9e7b3b359149a8d3f91521ebfbf4626
Maria Kim <80354714+mariaykim@users.noreply.github.com>
2024-03-11 14:48:59 -0400

    inital project creation

---
+++ TempoMusic/TempoMusic/TempoMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempoMusicApp.swift
+//  TempoMusic
+//
+//  Created by Maria Kim on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TempoMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
