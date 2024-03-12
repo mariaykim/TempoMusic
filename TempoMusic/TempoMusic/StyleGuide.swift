f49143cd7d0cccefd4c318823a6e1ef9de5c7423
Maria Kim <80354714+mariaykim@users.noreply.github.com>
2024-03-12 00:23:55 -0400

    Create StyleGuide.swift

---
+++ TempoMusic/TempoMusic/StyleGuide.swift
@@ -0,0 +1,19 @@
+//
+//  StyleGuide.swift
+//  TempoMusic
+//
+//  Created by Maria Kim on 3/11/24.
+//
+
+import Foundation
+
+struct StyleGuide {
+    
+    struct Size {
+        static let extraSmallPadding: CGFloat = 5
+        static let smallPadding: CGFloat = 10
+        static let mediumPadding: CGFloat = 20
+        static let largePadding: CGFloat = 30
+    }
+    
+}
